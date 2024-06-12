2b9d6413006d499f152029f211e5a8444db74825
shinYuki0214 <sin2022web@gmail.com>
2024-06-12 12:08:48 +0900

    new

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -6,6 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <style>
+        td,
+        th {
+            border: 1px solid black;
+            padding: 8px;
+        }
+    </style>
 </head>
 
 <body>
